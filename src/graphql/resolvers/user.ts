a2487602e37114d49d14848b14b6e3c25d911032
shogo4131 <shougo08697118@gmail.com>
2023-01-09 16:38:08 +0900

    feat: add register validation error

--- src/graphql/resolvers/user.ts
+++ src/graphql/resolvers/user.ts
@@ -1,18 +1,39 @@
+import { UserInputError } from "apollo-server";
 import * as bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 import User, { type UserDoc } from "../../models/User";
+import { vaidationRegisterInput } from "../../utils/validatitons";
 
 type UserRequest = {
   confirmPassword: string;
 } & UserDoc;
 
+// TODO: anyをなんとかする
 export const userResolvers = {
   Mutation: {
     async register(
       _: any,
       { registerInput: { username, email, password, confirmPassword } }: any
     ) {
+      const { errors, valid } = vaidationRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+
+      if (!valid) throw new UserInputError("errors", { errors });
+
+      const user = await User.findOne({ username });
+
+      // NOTE: 同じユーザー名の場合
+      if (user) {
+        throw new UserInputError("username is taken", {
+          errors: { username: "this username is taken" },
+        });
+      }
+
       password = await bcrypt.hash(password, 12);
 
       const newUser = new User({
