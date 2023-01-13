41131f42894c1c83dc9d452bd3f1964e5c2277ef
shogo4131 <shougo08697118@gmail.com>
2023-01-14 00:10:50 +0900

    feat: add comment and like type

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -6,6 +6,20 @@ export const typeDefs = gql`
     body: String!
     createdAt: String!
     username: String!
+    comments: [Comment]!
+    likes: [Like]!
+  }
+
+  type Comment {
+    id: ID!
+    username: String!
+    body: String!
+  }
+
+  type Like {
+    id: ID!
+    createdAt: String!
+    username: String!
   }
 
   type User {
@@ -32,5 +46,8 @@ export const typeDefs = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: ID!, body: String): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
   }
 `;
