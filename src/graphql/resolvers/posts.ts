125c093f349e6b87105d7c5720ba751aad00445e
shogo4131 <shougo08697118@gmail.com>
2023-01-13 16:13:54 +0900

    feat: add get post api and create post api

--- src/graphql/resolvers/posts.ts
+++ src/graphql/resolvers/posts.ts
@@ -1,7 +1,11 @@
 import Post from '../../models/Posts';
+import { authCheck } from '../../utils/auth-check';
 
 export const postsResolvers = {
   Query: {
+    /**
+     * 投稿一覧
+     */
     async getPosts() {
       try {
         const posts = await Post.find();
@@ -9,6 +13,38 @@ export const postsResolvers = {
       } catch (error) {
         if (error instanceof Error) console.error(error);
       }
+    },
+    /**
+     * 投稿
+     */
+    async getPost(_: any, { postId }: { postId: string }) {
+      try {
+        const post = await Post.findById(postId);
+
+        if (!post) throw new Error('投稿が見つかりませんでした。');
+
+        return post;
+      } catch (error) {
+        if (error instanceof Error) console.error(error);
+      }
+    },
+  },
+  /**
+   * 投稿を追加
+   */
+  Mutation: {
+    async createPost(_: any, { body }: { body: string }, context: any) {
+      const user = authCheck(context);
+
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+      });
+
+      const posts = await newPost.save();
+
+      return posts;
     },
   },
 };
