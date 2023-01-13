37fb46f2057532cac001fac4c3f5be3dd63b370d
shogo4131 <shougo08697118@gmail.com>
2023-01-14 02:30:01 +0900

    feat: add delete comment mutation

--- src/graphql/resolvers/comment.ts
+++ src/graphql/resolvers/comment.ts
@@ -1,10 +1,13 @@
-import { UserInputError } from 'apollo-server';
+import { AuthenticationError, UserInputError } from 'apollo-server';
 import { authCheck } from '../../utils/auth-check';
 import Post from '../../models/Posts';
 import User from '../../models/User';
 
 export const commentResolvers = {
   Mutation: {
+    /**
+     * コメント追加
+     */
     async createComment(_: any, { postId, body }: { postId: string; body: string }, context: any) {
       const user = authCheck(context);
 
@@ -16,6 +19,7 @@ export const commentResolvers = {
         });
       }
 
+      // TODO: Promise Allに修正
       const post = await Post.findById(postId);
       const selectedUser = await User.findById(user.id);
 
@@ -23,6 +27,34 @@ export const commentResolvers = {
       if (!selectedUser) throw new Error('該当のユーザーが見つかりませんでした。');
 
       post.comments.unshift({ body, username: selectedUser.username });
+
+      await post.save();
+
+      return post;
+    },
+    /**
+     * コメント削除
+     */
+    async deleteComment(
+      _: any,
+      { postId, commentId }: { postId: string; commentId: string },
+      context: any
+    ) {
+      const user = authCheck(context);
+
+      const post = await Post.findById(postId);
+      const selectedUser = await User.findById(user.id);
+
+      if (!post) throw new Error('投稿が見つかりませんでした。');
+      if (!selectedUser) throw new Error('該当のユーザーが見つかりませんでした。');
+
+      const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+
+      if (post.comments[commentIndex].username !== selectedUser.username) {
+        throw new AuthenticationError('削除が許可されていません。');
+      }
+
+      post.comments.splice(commentIndex, 1);
 
       await post.save();
 
