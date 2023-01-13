848f58a2147b967bda18f5f892e4512bc4bc452c
shogo4131 <shougo08697118@gmail.com>
2023-01-14 00:09:36 +0900

    refactor: post schema model

--- src/models/Posts.ts
+++ src/models/Posts.ts
@@ -3,9 +3,30 @@ import { model, Schema } from 'mongoose';
 interface PostsDoc {
   body: string;
   username: string;
-  commemts: string[];
+  comments: Commnet[];
   likes: string[];
 }
+
+interface Commnet {
+  body: string;
+  username: string;
+}
+
+const commentsSchama = new Schema(
+  {
+    body: { type: String },
+    username: { type: String },
+  },
+  { timestamps: true }
+);
+
+const likesSchema = new Schema(
+  {
+    username: { type: String },
+    createdAt: { type: String },
+  },
+  { timestamps: true }
+);
 
 const postsSchema = new Schema(
   {
@@ -16,10 +37,9 @@ const postsSchema = new Schema(
     username: {
       type: String,
       required: true,
-      unique: true,
     },
-    comments: [{ body: String, name: String, createdAt: String }],
-    likes: [{ name: String, createdAt: String }],
+    comments: [commentsSchama],
+    likes: [likesSchema],
     user: {
       type: Schema.Types.ObjectId,
       ref: 'users',
