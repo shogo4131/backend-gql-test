ea3b0d1e1c60ca0dc56d21ec7c939ef4e8a64cd2
shogo4131 <shougo08697118@gmail.com>
2023-01-15 15:10:00 +0900

    feat: add total count

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,8 +1,13 @@
 import { postsResolvers } from './posts';
 import { userResolvers } from './user';
 import { commentResolvers } from './comment';
+import type { PostsDoc } from '../../models/Posts';
 
 export const resolvers = {
+  Post: {
+    likeCount: (parent: PostsDoc) => parent.likes.length,
+    commentCount: (parent: PostsDoc) => parent.comments.length,
+  },
   Query: {
     ...postsResolvers.Query,
   },
